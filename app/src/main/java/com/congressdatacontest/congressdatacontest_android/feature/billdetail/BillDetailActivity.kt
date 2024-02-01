a962a570eec895c934da6406ca6b87d4493ed922
thsamajiki <ant1102@naver.com>
2024-02-02 01:54:17 +0900

    의안 현황 목록에서 의안 클릭했을 때 데이터가 넘어가도록 세팅

--- app/src/main/java/com/congressdatacontest/congressdatacontest_android/feature/billdetail/BillDetailActivity.kt
+++ app/src/main/java/com/congressdatacontest/congressdatacontest_android/feature/billdetail/BillDetailActivity.kt
@@ -1,10 +1,12 @@
 package com.congressdatacontest.congressdatacontest_android.feature.billdetail
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.congressdatacontest.congressdatacontest_android.databinding.ActivityBillDetailBinding
+import com.congressdatacontest.congressdatacontest_android.feature.board.Bill
 
 class BillDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBillDetailBinding
@@ -19,13 +21,25 @@ class BillDetailActivity : AppCompatActivity() {
         initListeners()
     }
 
+//    private fun getBillData(): Bill? {
+//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            intent.getParcelableExtra("bill", Bill::class.java)
+//        } else {
+//            @Suppress("DEPRECATION")
+//            intent.getParcelableExtra("bill")
+//        }
+//    }
+
     private fun initView() {
-//        binding.tvBillSuggesterValue.text
-//        binding.tvBillSuggesterDateValue.text
-//        binding.tvBillStatusValue.text
-//        binding.tvBillDetailContentValue.text
-//        binding.tvBillParentCategory.text
-//        binding.tvBillSubCategory.text
+//        binding.tvBillSuggesterValue.text = getBillData()?.suggester
+//        binding.tvBillSuggesterDateValue.text = getBillData()?.date
+//        binding.tvBillStatusValue.text = getBillData()?.status
+//        binding.tvBillDetailContentValue.text = getBillData()?.content
+//        binding.tvBillParentCategory.text = getBillData()?.majorTagName
+//        binding.tvBillSubCategory.text = getBillData()?.minorTagName
+//        binding.tvLikes.text = getBillData()?.likes.toString()
+//        binding.tvNotKnow.text = getBillData()?.noResponses.toString()
+//        binding.tvDislikes.text = getBillData()?.dislikes.toString()
     }
 
     private fun initListeners() {
